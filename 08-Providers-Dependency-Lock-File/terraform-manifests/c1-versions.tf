989e9cd56621b95146136f32bc7f951f30c5d26f
Muriel Boutefeu <MURB@equinor.com>
2025-01-14 15:28:00 +0100

    ex8 with client_id

--- 08-Providers-Dependency-Lock-File/terraform-manifests/c1-versions.tf
+++ 08-Providers-Dependency-Lock-File/terraform-manifests/c1-versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      #version = "1.44.0"
-      version = ">= 2.0" 
+      version = "1.44.0"
+      #version = ">= 2.0" 
     }
     random = {
       source = "hashicorp/random"
-      version = ">= 3.0"
+      version = ">=3.1.0"
     }
   }
 }
@@ -17,7 +17,10 @@ terraform {
 # Provider Block
 provider "azurerm" {
    features {}
-   subscription_id = "eee0563a-6259-42f3-b289-6d201645b2b5"        
+   subscription_id = "eee0563a-6259-42f3-b289-6d201645b2b5"  
+   client_id = "8aea866f-3a4f-45b2-8eca-2058d1d7c977"
+   client_secret = "to_include"  
+   tenant_id =  "82121e4c-9dc6-42ff-a26f-222dbc796cc6" 
 }
 
 
