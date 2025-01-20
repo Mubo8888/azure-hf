63e3b3e05f86e5ce0864d6cd6304c065b9710cc3
Muriel Boutefeu <MURB@equinor.com>
2025-01-20 14:31:57 +0100

    ex47

---
+++ AzPowerShell.ps1
@@ -0,0 +1,21 @@
+Get-PSRepository
+
+Get-Module Az* -ListAvailable
+
+Find-Module -Name Az 
+
+Install-Module az -Scope AllUsers -Force
+
+Update-Module Az
+
+Connect-AzAccount
+
+Connect-AzAccount -UseDeviceAuthentication
+
+Get-Alias add-azaccount
+Get-Alias login-azaccount
+
+Connect-AzAccount -Identity 
+connect-AzAccount -Tenant .... 
+
+Get-AzContext
